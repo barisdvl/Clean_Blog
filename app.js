469397c602b0fa0e1d605c4009dfd7602195bfbd
barisdvl <barisdeveli48@gmail.com>
2021-12-30 22:11:27 +0300

    2.bölüm Ödev 8 Uploaded

--- app.js
+++ app.js
@@ -3,10 +3,20 @@ const app = express();
 
 const port = 3000;
 
-const blog = { id: 1, title: 'Blog title', description: 'Blog description' };
+app.use(express.static('public'))
+
+app.set("view engine", "ejs")
 
 app.get('/', (req, res) => {
-  res.send(blog);
+  res.render('index');
+});
+
+app.get('/about', (req, res) => {
+  res.render('about');
+});
+
+app.get('/add_post', (req, res) => {
+  res.render('add_post');
 });
 
 app.listen(port, () => {
